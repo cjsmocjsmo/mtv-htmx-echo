ac0a838bf2c9170c5b00b8df4aa15874f1e4a1be
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-04-27 13:29:17 -0700

    Add movie genre templates and routes

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	// "crypto/tls"
 	// "golang.org/x/crypto/acme"
 	// "database/sql"
 	// "flag"
-	// "fmt"
+	"fmt"
 	"html/template"
 	"io"
 	// "log"
-	// "mime/multipart"
+	
 	"net/http"
-	// "net/mail"
-	// "os"
+	
+	"os"
 	// "regexp"
 	// "strconv"
 	// "time"
@@ -23,132 +22,59 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	// "github.com/mailjet/mailjet-apiv3-go/v4"
 	// _ "github.com/mattn/go-sqlite3"
-	// "golang.org/x/crypto/acme/autocert"
 )
 
 type Template struct {
 	templates *template.Template
 }
 
-// func createAccountsDB(db_path string) {
-// 	db, err := sql.Open("sqlite3", db_path)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer db.Close()
-// 	sqlStmt := `
-// 	CREATE TABLE IF NOT EXISTS accounts (
-// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-// 		acctid TEXT NOT NULL UNIQUE,
-// 		email TEXT NOT NULL UNIQUE,
-// 		date TEXT NOT NULL
-// 	);
-// 	`
-// 	_, err = db.Exec(sqlStmt)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+type MovieStruct struct {
+	Name 			string
+	Year 			string
+	PosterAddr 		string
+	Size 			string
+	Path 			string
+	Idx 			string
+	MovId 			string
+	Catagory 		string
+	HttpThumbPath 	string
+}
 
-// func createCommentsDB(db_path string) {
-// 	db, err := sql.Open("sqlite3", db_path)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer db.Close()
-// 	sqlStmt := `
-// 	CREATE TABLE IF NOT EXISTS comments (
-// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-// 		acctid TEXT NOT NULL,
-// 		comid TEXT NOT NULL,
-// 		name TEXT NOT NULL,
-// 		email TEXT NOT NULL,
-// 		rating TEXT NOT NULL,
-// 		comment TEXT NOT NULL,
-// 		date TEXT NOT NULL,
-// 		media TEXT NOT NULL,
-// 		status TEXT NOT NULL
-// 	);
-// 	`
-// 	_, err = db.Exec(sqlStmt)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
+type TvShowStruct struct {
+	TvId 			string
+	Size			string
+	Catagory		string
+	Name 			string
+	Season 			string
+	Episode 		string
+	Path 			string
+	Idx 			string
+}
 
-// func createEstimatesDB(db_path string) {
-// 	db, err := sql.Open("sqlite3", db_path)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer db.Close()
-// 	sqlStmt := `
-// 	CREATE TABLE IF NOT EXISTS estimates (
-// 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-// 		acctid TEXT NOT NULL,
-// 		estid TEXT NOT NULL,
-// 		name TEXT NOT NULL,
-// 		address TEXT NOT NULL,
-// 		city TEXT NOT NULL,
-// 		phone TEXT NOT NULL,
-// 		email TEXT NOT NULL,
-// 		servdate TEXT NOT NULL,
-// 		recdate TEXT NOT NULL,
-// 		comment TEXT NOT NULL,
-// 		media TEXT NOT NULL,
-// 		status TEXT NOT NULL
-// 	);
-// 	`
-// 	_, err = db.Exec(sqlStmt)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-// func createUploadsDir(uploads_path string) {
-// 	_, err := os.Stat(uploads_path)
-// 	if os.IsNotExist(err) {
-// 		err := os.MkdirAll(uploads_path, 0755)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			fmt.Print("unable to create uploads dir")
-// 		}
-// 	}
-// }
-
-// func dbFileExists(db_path string) bool {
-// 	_, err := os.Stat(db_path)
-// 	return os.IsNotExist(err) 
-// }
-
-// func createCertDir(cert_path string) {
-// 	err := os.MkdirAll(cert_path, 0755)
-// 	if err != nil {
-// 		fmt.Println(err)
-// 		fmt.Print("unable to create cert dir")
-// 	}
-// }
+func checkDBExists() {
+	mtvDBPath := os.Getenv("MTV_DB_PATH")
+	if _, err := os.Stat(mtvDBPath); os.IsNotExist(err) {
+		// file does not exist
+		fmt.Println("Database file does not exist\n Please run mtvsetup.")
+		os.Exit(1)
+	} else if err != nil {
+		// other error
+		fmt.Println("Error checking for database file: ", err)
+		os.Exit(1)
+	}
+	// file exists
+	fmt.Println("Database file exists.")
+}
 
 func init() {
 
-	// godotenv.Load("atshtmxecho.env")
-	godotenv.Load("test.env")
+	godotenv.Load("mtvhtmxecho.env")
+	// checkDBExists()
 
-	// if !dbFileExists(os.Getenv("ATS_DB_PATH")) {
-	// 	log.Println("DB file does not exist creating it")
-		// dbpath := os.Getenv("ATS_DB_PATH")
-		// createAccountsDB(dbpath)
-		// createCommentsDB(dbpath)
-		// createEstimatesDB(dbpath)
-	// }
+	
 
-	// uploadsPath := os.Getenv("ATS_UPLOADS_PATH")
-	// createUploadsDir(uploadsPath)
-
-	// certpath := os.Getenv("ATS_CERT_PATH")
-	// createCertDir(certpath)
+	
 
 	// filePath := os.Getenv("ATS_DB_PATH")
 	// _, err3 := os.OpenFile(filePath, os.O_CREATE|os.O_EXCL, 0755)
@@ -177,6 +103,11 @@ func main() {
 
 		e.GET("/", mtv_index)
 		e.GET("/movies", mtv_movies)
+		e.GET("/movaction", mov_action)
+		e.GET("/movarnold", mov_arnold)
+		e.GET("/movbrucelee", mov_brucelee)
+		e.GET("/movbrucewillis", mov_brucewillis)
+
 		e.GET("/tvshows", mtv_tvshows)
 		e.GET("/tvaction", tv_action)
 		e.GET("/tvcomedy", tv_comedy)
@@ -196,6 +127,34 @@ func main() {
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
+
+func mov_action(c echo.Context) error {
+	return c.Render(http.StatusOK, "mov_action", "WORKED")
+}
+
+func mov_arnold(c echo.Context) error {
+	return c.Render(http.StatusOK, "mov_arnold", "WORKED")
+}
+
+func mov_brucelee(c echo.Context) error {
+	return c.Render(http.StatusOK, "mov_brucelee", "WORKED")
+}
+
+func mov_brucewillis(c echo.Context) error {
+	return c.Render(http.StatusOK, "mov_brucewillis", "WORKED")
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 func mtv_index(c echo.Context) error {
 	return c.Render(http.StatusOK, "mtv_index", "WORKED")
